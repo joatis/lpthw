7ac75be5657decdc215433e02316aefe2a7ff87e
John Oatis <joatis@joatis3.com>
2024-01-18 22:13:24 -0500

    Commented ex9

---
+++ ex9.py
@@ -0,0 +1,20 @@
+# Here's some new strange stuff, remember type it exactly.
+
+# define days as a string
+days = "Mon Tue Wed Thu Fri Sat Sun"
+# define months as a string using \n to printt new-line breaks.
+months = "Jan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug"
+
+# print a string and then days as arguments
+print("Here are the days: ", days)
+# print a string and then months as arguments
+print("Here are the months: ", months)
+
+# Print a string using triple quotes to keep format of string.
+print("""
+There's something going on here.
+With the three double-quotes.
+We'll be able to type as much as we like.
+Even 4 lines if we want, or 5 or 6.
+""")
+      
