bb0d5473340561ba6afc67d3a05c3cbeec0b0096
John Oatis <joatis@joatis3.com>
2024-01-18 21:49:06 -0500

    Commented ex7

--- ex7.py
+++ ex7.py
@@ -1,8 +1,13 @@
+# Print a string
 print("Mary had a little lamb.")
-print("Its fleece was white as {}".format({'snow'}))
+# Print a string that uses the format string method to interpolate.
+print("Its fleece was white as {}".format('snow'))
+# Print a string
 print("And everywhere that Mary went.")
+# Print a charater 10 times
 print("." * 10) # what'd it do?
 
+# assign each variable a character value.
 end1 = "C"
 end2 = "h"
 end3 = "e"
@@ -17,5 +22,9 @@ end11 = "e"
 end12 = "r"
 
 # watch end = ' ' at the end. try removing it to see what happens
+# The second arg is a key word param called end. Usually it defaults to a 
+# \n but here we are overriding it with a space that causes both print statements
+# to be written on the same line.
+# Print the concatented values using 2 separate print statements.
 print(end1 + end2 + end3 + end4 + end5 + end6, end=' ')
 print(end7 + end8 + end9 + end10 + end11 + end12)
