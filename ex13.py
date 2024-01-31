72a905230b58e4f53cc98692812eb4b379f68c37
John Oatis <joatis@joatis3.com>
2024-01-30 21:31:05 -0500

    added ex13 using argv

---
+++ ex13.py
@@ -0,0 +1,10 @@
+from sys import argv
+# read the WYSS section for how to run this
+script, first, second, third = argv
+fourth = input("Give a fourth variable:")
+print("The script is called:", script)
+print("The first variable is:", first)
+print("The second variable is:", second)
+print("The third variable is:", third)
+print("The fourth variable is:", fourth)
+
