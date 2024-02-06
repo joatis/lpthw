125bf6f6c54dfcc89ae05e0cd32eeb74c2db073b
John Oatis <joatis@joatis3.com>
2024-02-05 22:05:55 -0500

    commented ex15.py

--- ex15.py
+++ ex15.py
@@ -1,15 +1,16 @@
+# inport the argv from sys to get arguments.
 from sys import argv
-
+# unpack argv into two variables.
 script, filename = argv
-
+# open the filename in argv as a text file
 txt = open(filename)
-
+# print the filename and the files contents after reading
 print(f"Here is your file {filename}:")
 print(txt.read())
-
+# Prompt the user to re-enter the filename
 print("Type the filename again:")
 file_again = input("> ")
-
+# Open another file handle.
 txt_again = open(file_again)
-
+# print the contents of the file from the new filehandle.
 print(txt_again.read())
