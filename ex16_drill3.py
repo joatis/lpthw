848aad8e32d90c5072ccbe1d58423226e2c6862f
John Oatis <joatis@joatis3.com>
2024-02-10 10:51:58 -0500

    ex 16 and drills opene and writing to files

---
+++ ex16_drill3.py
@@ -0,0 +1,33 @@
+from sys import argv
+
+script, filename = argv
+
+print(f"We're going to erase {filename}.")
+print("Its contents are: ")
+print(open(filename).read())
+print("If you don't want that, hit CTRL-C (^C).")
+print("If you do want that, hit RETURN")
+
+input('?')
+
+print("Opening the file...")
+target = open(filename, 'w')
+
+print("Truncating the file. Goodbye!")
+target.truncate()
+
+print("Now I'm going to ask you for three lines.")
+
+line1 = input("line 1: ")
+line2 = input("line 2: ")
+line3 = input("line 3: ")
+
+print("I'm going to write these tio the file.")
+target.write(f"""
+{line1}
+{line2}
+{line3}
+""")
+
+print("And finially we close it.")
+target.close()
