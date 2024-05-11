7de75b526367e35c20c79d1c1909095f4e649ace
John Oatis <joatis@joatis3.com>
2024-05-11 10:23:07 -0400

    Made nums variable

--- ex33.py
+++ ex33.py
@@ -1,14 +1,22 @@
 i = 0
 numbers = []
 
-while i < 6:
-  print(f"At the top i is {i}")
-  numbers.append(i)
+def print_numbers(limit, step):
+  global i
+  while i < limit:
+    print(f"At the top i is {i}")
+    numbers.append(i)
 
-  i = i + 1
-  print("Numbers now: ", numbers)
+    i = i + step
+    print("Numbers now: ", numbers)
+
+number_limit = int(input("Enter the max number to count up to:"))
+increase_by = int(input("Amount to increase each number by: "))
+print_numbers(number_limit, increase_by)
 
 print("The numbers: ")
 
 for num in numbers:
   print(num)
+
+
