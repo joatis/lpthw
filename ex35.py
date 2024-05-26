1ac702389e17e5748953dee52da73a43957b80a2
John Oatis <joatis@joatis3.com>
2024-05-26 11:57:06 -0400

    ex35 replaced bad if test with innumeric

--- ex35.py
+++ ex35.py
@@ -4,7 +4,8 @@ def gold_room():
   print("This room is full of gold.  How much do you take?")
 
   choice = input("> ")
-  if "0" in choice or "1" in choice:
+  ## Fixing the weird if test for 0 or 1 with the isnumeric function
+  if choice.isnumeric:
     how_much = int(choice)
   else:
     dead("Man, learn to type a number,")
