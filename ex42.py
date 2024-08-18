e6843f1281296508cd31d451b71ee6ff67c327a5
John Oatis <joatis@joatis3.com>
2024-08-18 11:35:44 -0400

    ex42.py is-a and has-a relationship Pt -2 is- and has-a defined

--- ex42.py
+++ ex42.py
@@ -2,21 +2,21 @@
 class Animal(object):
   pass
 
-## ??
+## ?? Dog is-a Animal
 class Dog(Animal):
 
   def __init__(self, name):
     ##??
     self.name = name
 
-## ??
+## ?? Cat is-a Animal
 class Cat(Animal):
 
   def __init__(self, name):
     ## ??
     self.name = name
 
-## ??
+## ?? Person is-a object and has-a name and pet 
 class Person(object):
 
   def __init__(self, name):
@@ -26,7 +26,7 @@ class Person(object):
     # Person has-a pet of some kind
     self.pet = None
 
-## ??
+## ?? Employee is-a person that has-a salary
 class Employee(Person):
 
   def __init__(self, name, salary):
@@ -35,41 +35,41 @@ class Employee(Person):
     ## ??
     self.salary = salary
 
-## ??
+## ?? Fish is-a object
 class Fish(object):
   pass
 
-## ??
+## ?? Salmon is-a Fish
 class Salmon(Fish):
   pass
 
-## ?? 
+## ?? Halibut is-a Fish
 class Halibut(Fish):
   pass
 
 ## rover is-a Dog
 rover = Dog("Rover")
 
-## ??
+## ?? satan is-a Cat
 satan = Cat("Satan")
 
-## ??
+## ?? mary is-a Person
 mary = Person("Mary")
 
-## ??
+## ?? mary has-a pet Cat satan
 mary.pet = satan
 
-## ??
+## ?? frank is-a Employee that has-a salary of 120000
 frank = Employee("Frank", 120000)
 
-## ??
+## ?? frank has-a pet Dog rover
 frank.pet = rover
 
-## ??
+## ?? flipper is-a Fish
 flipper = Fish()
 
-## ??
+## ?? crouse is-a Salmon
 crouse = Salmon()
 
-## ??
+## ?? harry is-a Halibut
 harry = Halibut()
