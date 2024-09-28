1b16e8d1a71fa37ea8f6b0a4f04f24de3ca0b3e6
John Oatis <joatis@joatis3.com>
2024-09-28 11:44:01 -0400

    Added escapepod and finished scene

--- ex43.py
+++ ex43.py
@@ -184,7 +184,49 @@ class TheBridge(Scene):
       print("DOES NOT COMPUTE!")
       return "the_bridge"
     
+class EscapePod(Scene):
 
+  def enter(self):
+    print(dedent("""
+          You rush through the ship desperately trying to make it to
+          the escape pod before the whole ship explodes. It seems 
+                 like hardly any Gothons are on the ship, so your run is
+                 clear of interferance. You get to the chamber with the escape pods
+                 and now need to pick one to take. Some iof them code be damaged
+                 but you don't have time to look.
+                 There's 5 pods which one do you take?
+                 """))
+    
+    good_pod = randint(1,5)
+    guess = input("[pod #]> ")
+
+    if int(guess) != good_pod:
+      print(dedent("""
+            You jump into pod {guess} and hit the eject button.
+            The pod escapes out into the void of space, then
+            implodes as teh hull ruptures, crushing your body into
+            jam jelly.
+            """))
+      return "death"
+    else:
+      print(dedent("""
+            You jump into pod {guess} and hit the eject button.
+            The pod easily slides out into space heading to the 
+            planet below. As it flies to the planet, you look
+            back and see your ship implode than explode like a 
+            bright star, taking out the Gothon ship at the same
+            time. You won!
+            """))
+      
+    return 'finished'
+  
+
+class Finished(Scene)
+  
+  def enter(self):
+    print("yopu won! Good job.")
+    return 'finished'
+  
     
 
     
